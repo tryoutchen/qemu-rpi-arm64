dd66335a0ce0dd50494c9303bb3f253cca577d5e
Tryout Chen <tryout.chen@gmail.com>
2022-02-12 16:14:30 +0800

    increase ci timeout and refine run_qemu

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eo pipefail
+set -eo pipefail -x
 
 raspberrypi_image="2022-01-28-raspios-bullseye-arm64-lite.img"
 
@@ -13,7 +13,7 @@ qemu-system-aarch64 \
   -m 256 -M virt \
   -cpu cortex-a53 \
   -append 'root=/dev/vda2 rw panic=1' \
-  -drive "file=2022-01-28-raspios-bullseye-arm64-lite.img,if=none,index=0,media=disk,format=raw,id=disk0" \
+  -drive "file=${raspberrypi_image},if=none,index=0,media=disk,format=raw,id=disk0" \
   -device "virtio-blk-pci,drive=disk0,disable-modern=on,disable-legacy=off" \
   -device "virtio-net-pci,netdev=net0" \
   -netdev "user,id=net0,hostfwd=tcp::5022-:22" \
